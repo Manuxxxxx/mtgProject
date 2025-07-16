9e25673726f42a0c0e9a7c0fb68d302cde19c2bf
Emanuele Ricca <emanuele.ricca.10@gmail.com>
2025-07-17 01:23:41 +0200

    changed balanced loss for focal and ce

--- joint_train.py
+++ joint_train.py
@@ -106,6 +106,34 @@ def set_seed(seed: int = 42):
     # torch.backends.cudnn.benchmark = False
 
 
+class LossScaler:
+    def __init__(self, smoothing=0.95, epsilon=1e-8, initial_scale=1.0):
+        self.avg_tag_loss = None
+        self.avg_synergy_loss = None
+        self.smoothing = smoothing
+        self.epsilon = epsilon
+        self.initial_scale = initial_scale
+
+    def update(self, tag_loss_val, synergy_loss_val):
+        if self.avg_tag_loss is None:
+            self.avg_tag_loss = tag_loss_val
+            self.avg_synergy_loss = synergy_loss_val
+        else:
+            self.avg_tag_loss = self.smoothing * self.avg_tag_loss + (1 - self.smoothing) * tag_loss_val
+            self.avg_synergy_loss = self.smoothing * self.avg_synergy_loss + (1 - self.smoothing) * synergy_loss_val
+
+    def get_scaled_total_loss(self, tag_loss, synergy_loss, alpha=1.0):
+        self.update(tag_loss.item(), synergy_loss.item())
+        # Compute scale factor to bring tag loss to same level as synergy loss
+        if self.avg_tag_loss < self.epsilon:
+            scale_factor = self.initial_scale
+        else:
+            scale_factor = self.avg_synergy_loss / (self.avg_tag_loss + self.epsilon)
+
+        scaled_tag_loss = scale_factor * tag_loss
+        return synergy_loss + alpha * scaled_tag_loss, scale_factor
+
+
 class CardDataset(Dataset):
     def __init__(self, card_data, tokenizer, max_length=320, tags_len=None, dataset_name="tag", tag_to_index_file=None):
         
@@ -807,7 +835,9 @@ def train_multitask_loop(
                 synergy_model, embed1, embed2, projected_tag_embed1, projected_tag_embed2, labels_synergy, loss_synergy_model, false_positive_penalty
             )
 
-            full_loss = weighted_loss_synergy + tag_loss_weight * tag_loss
+            # full_loss = weighted_loss_synergy + tag_loss_weight * tag_loss
+            loss_scaler = LossScaler()
+            full_loss, scale_factor = loss_scaler.get_scaled_total_loss(tag_loss, weighted_loss_synergy)
             loss_scaled = full_loss / accumulation_steps
 
         scaler.scale(loss_scaled).backward()
